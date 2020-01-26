c35a288804963e55ff4b2618c87d1998ebe1619a
W. David Porter <david.porter@fastmail.com>
2020-01-27 06:18:35 +1000

    collate data for first table

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -1,19 +1,33 @@
-var a = regionInfoTemplate;
 
-print(a);
+//print(String.format(regionInfoTemplate, "queensland", "heading", 0, "", "", "", "", "", ""));
 
-print(String.format(regionInfoTemplate, "queensland", "heading", 0, "", "", "", "", "", ""));
-
-var b = '%globals_asset_file_contents:75^replace:\r\n:\\n%';
-
-print("<hr>");
-
+var csv = '%globals_asset_file_contents:75^replace:\r\n:\\n%';
 var results = Papa.parse(
-    b, 
-    {
-        skipEmptyLines: true, 
-        dynamicTyping: true
-    }
+	csv,
+	{
+		skipEmptyLines: true,
+		dynamicTyping: true
+	}
 );
 
-print(JSON.stringify(results));
+var headData = results.data[0];
+var data = results.data.slice(1);
+
+var groups = {};
+data.forEach(function(record) {
+	var group = String.format("{0}. {1}", record[1], record[2]);
+	if (!groups[group]) {
+		groups[group] = { p: 0, np: 0 };
+	}
+	groups[group].p += record[4];
+	groups[group].np += record[5];
+});
+
+print("<hr>");
+print(JSON.stringify(groups));
+
+//print(String.format(regionInfoTemplate, "Hectares of broad vegetation groups in protected areas, 2015", "heading", 0, "", "", "", "", "", ""));
+
+
+print("<hr>");
+print(JSON.stringify(data));
