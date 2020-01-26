e90e5e84c001575e5d3e088dc4e515a835e1c3e7
David Porter <david.porter@fastmail.com>
2020-01-27 05:19:17 +1000

    typo

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -4,10 +4,12 @@ print(x);
 
 print(String.format(x, "this is a heading", "", "", "", "", "", ""));
 
-var results = Papa.parse("%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js
-^replace:\r\n:\\n%", {
-    skipEmptyLines: true, 
-    dynamicTyping: true
-});
+var results = Papa.parse(
+    "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js^replace:\r\n:\\n%", 
+    {
+        skipEmptyLines: true, 
+        dynamicTyping: true
+    }
+);
 
 print(JSON.stringify(results));
