db07a18d0da9120ec4641c8ed9d92317905d08ae
David Porter <david.porter@fastmail.com>
2020-01-27 05:21:46 +1000

    test fetching asset file contents

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -1,15 +1,20 @@
-var x = regionInfoTemplate;
+var a = regionInfoTemplate;
 
-print(x);
+print(a);
 
-print(String.format(x, "this is a heading", "", "", "", "", "", ""));
+print(String.format(a, "this is a heading", "", "", "", "", "", ""));
 
-var results = Papa.parse(
-    "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js^replace:\r\n:\\n%", 
-    {
-        skipEmptyLines: true, 
-        dynamicTyping: true
-    }
-);
+var b = "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js";
 
-print(JSON.stringify(results));
+print (b);
+
+
+//var results = Papa.parse(
+//    "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js^replace:\r\n:\\n%", 
+//    {
+//        skipEmptyLines: true, 
+//        dynamicTyping: true
+//    }
+//);
+
+//print(JSON.stringify(results));
