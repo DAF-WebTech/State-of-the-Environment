cab514d34936f43546521ac5bedd47713eabc4bc
David Porter <david.porter@fastmail.com>
2020-01-27 05:10:18 +1000

    add papa parse

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -3,3 +3,10 @@ var x = regionInfoTemplate;
 print(x);
 
 print String.format(x, "this is a heading", "", "", "", "", "", "");
+
+var results = Papa.parse("%globals_asset_file_contents:75^replace:\r\n:\\n%", {
+    skipEmptyLines: true, 
+    dynamicTyping: true
+});
+
+print(JSON.stringify(results));
