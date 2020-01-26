8a275b3b70d29f1e515abbe3cf5fddb040be1a58
David Porter <david.porter@fastmail.com>
2020-01-27 05:26:42 +1000

    use pipe as delimiter for replace modifier

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -4,7 +4,7 @@ print(a);
 
 print(String.format(a, "this is a heading", "", "", "", "", "", ""));
 
-var b = "%globals_asset_file_contents:75%^replace:\r\n:\\n";
+var b = "%globals_asset_file_contents:75%^replace:\r\n:|";
 
 print (b);
 
