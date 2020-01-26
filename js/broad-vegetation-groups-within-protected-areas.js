2afbabc8d4ce2f3a24e0fb0cd3cdbc73ae5a3044
David Porter <david.porter@fastmail.com>
2020-01-27 05:22:27 +1000

    typo

--- js/broad-vegetation-groups-within-protected-areas.js
+++ js/broad-vegetation-groups-within-protected-areas.js
@@ -4,7 +4,7 @@ print(a);
 
 print(String.format(a, "this is a heading", "", "", "", "", "", ""));
 
-var b = "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js";
+var b = "%globals_asset_file_contents:70:js/broad-vegetation-groups-within-protected-areas.js%";
 
 print (b);
 
