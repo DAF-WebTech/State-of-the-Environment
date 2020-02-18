547aea1676b8617e084de33e5e1b59b659e5756a
PORTER David <david.porter@daf.qld.gov.au>
2020-02-18 16:15:57 +1000

    fix orientation of tables

--- js/biodiversity/terrestrial/land-clearing-impact-on-woody-native-vegetation.js
+++ js/biodiversity/terrestrial/land-clearing-impact-on-woody-native-vegetation.js
@@ -1,6 +1,8 @@
+var csv = '%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%';
 
-var results = Papa.parse("%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%", {
-	skipEmptyLines: true, 
+
+var results = Papa.parse(csv, {
+	skipEmptyLines: true,
 	dynamicTyping: true
 });
 
@@ -21,7 +23,7 @@ var chartData = [];
 var years = data[0];
 years = years.slice(2);
 years = years.map(function (year) {
-return year.replace("-", "–"); //&ndash; in years
+	return year.replace("-", "–"); //&ndash; in years
 });
 
 
@@ -39,24 +41,28 @@ table[0] = table[0].concat(years);
 
 //body
 for (var i = 1; i < data.length; ++i) {
-if (data[i][1] == "Total clearing") {
-	var row = [data[i][0]]// region name
-	row = row.concat(data[i].slice(2));
-	table.push(row);
-}
+	if (data[i][1] == "Total clearing") {
+		var row = [data[i][0]]// region name
+		row = row.concat(data[i].slice(2));
+		table.push(row);
+	}
 }
 
 // iterate array for our html table
-var tableHtml = tableToHtml(table, true);
+var tableData = table.transpose();
+var tableHtml = tableToHtml(tableData, false);
 
 
-var regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody, tableHtml.tfoot)
+var regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody)
 print(regionInfo);
 
 
 
 // transpose for chart
-var chart = { data: table.transpose() };
+var chart = {
+	data: tableData,
+	type: "column"
+};
 chart.data[0][0] = { label: "Year", type: "string" };
 chart.options = getDefaultColumnChartOptions(table.length);
 chart.options.hAxis.title = "Year range";
@@ -72,43 +78,46 @@ heading = "Proportion of replacement landcover (clearing type) "; // {1}
 // get our clearing types
 var excludes = ["Total clearing", "Non-remnant", "Remnant"];
 var clearingTypes = data.slice(1).reduce(function (acc, current) {
-if (excludes.indexOf(current[1]) == -1 && acc.indexOf(current[1]) == -1) {
-	acc.push(current[1]);
-	return acc;
-}
-else
-	return acc;
+	if (excludes.indexOf(current[1]) == -1 && acc.indexOf(current[1]) == -1) {
+		acc.push(current[1]);
+		return acc;
+	}
+	else
+		return acc;
 }, []);
 // create an 2d array to represent our table
 table = [["Clearing"]];
 table[0] = table[0].concat(years);
 //body
 var empties = years.map(function (y) {
-return null;
+	return null;
 });
 clearingTypes.forEach(function (ct) {
-var row = [ct].concat(empties);
-for (var i = 1; i < data.length; ++i) {
-	if (data[i][1] == ct) {
-		for (var j = 2; j < data[i].length; ++j) {
-			if (data[i][j] != null)
-				row[j - 1] += data[i][j];
+	var row = [ct].concat(empties);
+	for (var i = 1; i < data.length; ++i) {
+		if (data[i][1] == ct) {
+			for (var j = 2; j < data[i].length; ++j) {
+				if (data[i][j] != null)
+					row[j - 1] += data[i][j];
+			}
 		}
 	}
-}
-table.push(row);
+	table.push(row);
 });
 
 // iterate array for our html table
-tableHtml = tableToHtml(table, true);
+tableHtml = tableToHtml(table.transpose());
 
 // TODO matrix replace
-regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody, tableHtml.tfoot);
+regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody);
 print(regionInfo);
 
 
 // transpose for chart
-chart = { data: table.transpose() };
+chart = {
+	data: table.transpose(),
+	type: "column"
+};
 chart.data[0][0] = { label: "Year", type: "string" };
 chart.options = getDefaultColumnChartOptions();
 chart.options.hAxis.title = "Year range";
@@ -125,34 +134,36 @@ chartData.push(chart);
 // remnant, non-remnant
 heading = "Historic woody vegetation clearing in Queensland "; // {1}
 // get our clearing types
-var myClearingTypes = ["Remnant", "Non-remnant"];
-// create an 2d array to represent our table
-table = [["Historic"]];
-table[0] = table[0].concat(years);
-//body
-myClearingTypes.forEach(function (ct) {
-var row = [ct].concat(empties);
-for (var i = 1; i < data.length; ++i) {
-	if (data[i][1] == ct) {
-		for (var j = 2; j < data[i].length; ++j) {
-			if (data[i][j] != null)
-				row[j - 1] += data[i][j];
-		}
-	}
-}
-table.push(row);
+var remnants = data.filter(function (d) {
+	return d[1] == "Remnant" && d[2] != "";
 });
+var nonremnants = data.filter(function (d) {
+	return d[1] == "Non-remnant" && d[2] != "";
+});
+table = [["Year", "Remnant", "Non-remnant"]];
+for (var i = 12; i < data[0].length; ++i) {
+	var r = remnants.reduce(function (x, y) {
+		return x + y[i];
+	}, 0);
+	var nr = nonremnants.reduce(function (x, y) {
+		return x + y[i];
+	}, 0);
+	table.push([data[0][i], r, nr]);
+}
 
 // iterate array for our html table
-tableHtml = tableToHtml(table, true);
+tableHtml = tableToHtml(table);
 
 // TODO matrix replace
-regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody, tableHtml.tfoot)
+regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody)
 print(regionInfo);
 
 
 // transpose for chart
-chart = { data: table.transpose() };
+chart = {
+	data: table,
+	type: "column"
+};
 chart.data[0][0] = { label: "Year", type: "string" };
 chart.options = getDefaultColumnChartOptions();
 chart.options.hAxis.title = "Year range";
@@ -165,86 +176,93 @@ chartData.push(chart);
 
 // charts per region
 var regions = data.slice(1).reduce(function (acc, current) {
-if (acc.indexOf(current[0]) == -1) {
-	acc.push(current[0]);
-	return acc;
-}
-else
-	return acc;
+	if (acc.indexOf(current[0]) == -1) {
+		acc.push(current[0]);
+		return acc;
+	}
+	else
+		return acc;
 }, []);
 
 
 regions.forEach(function (regionName) {
 
-region = regionName.toKebabCase(); // {0}
-// chart 1 total clearing
-heading = "Proportion of replacement landcover (clearing type) in " + regionName; // {1}
-table = [["Type"]];
-table[0] = table[0].concat(years)
+	region = regionName.toKebabCase(); // {0}
+	// chart 1 total clearing
+	heading = "Proportion of replacement landcover (clearing type) in " + regionName; // {1}
+	table = [["Type"]];
+	table[0] = table[0].concat(years)
 
-//body
-for (var i = 1; i < data.length; ++i) {
-	if (data[i][0] == regionName && clearingTypes.indexOf(data[i][1]) > -1) {
-		var row = [data[i][1]]// clearing type
-		row = row.concat(data[i].slice(2));
-		table.push(row);
+	//body
+	for (var i = 1; i < data.length; ++i) {
+		if (data[i][0] == regionName && clearingTypes.indexOf(data[i][1]) > -1) {
+			var row = [data[i][1]]// clearing type
+			row = row.concat(data[i].slice(2));
+			table.push(row);
+		}
 	}
-}
 
-// iterate array for our html table
-tableHtml = tableToHtml(table, true);
+	// iterate array for our html table
+	var myTable = table.transpose();
+	tableHtml = tableToHtml(myTable);
 
-regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody, tableHtml.tfoot)
-print(regionInfo);
-
-
-// transpose for chart
-chart = { data: table.transpose() };
-chart.data[0][0] = { label: "Year", type: "string" };
-chart.options = getDefaultColumnChartOptions();
-chart.options.hAxis.title = "Year range";
-chart.options.vAxis.title = "Hectares";
-chart.options.isStacked = true;
-chartData.push(chart);
+	regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody)
+	print(regionInfo);
 
 
-// chart 2 total clearing
+	// transpose for chart
+	chart = {
+		data: myTable,
+		type: "column"
+	};
+	chart.data[0][0] = { label: "Year", type: "string" };
+	chart.options = getDefaultColumnChartOptions();
+	chart.options.hAxis.title = "Year range";
+	chart.options.vAxis.title = "Hectares";
+	chart.options.isStacked = true;
+	chartData.push(chart);
 
 
-// chart 1 total clearing
-heading = "Historic woody vegetation clearing in  " + regionName; // {1}
-table = [["Type"]];
-table[0] = table[0].concat(years)
+	// chart 2 total clearing
 
-//body
-for (var i = 1; i < data.length; ++i) {
-	if (data[i][0] == regionName && (data[i][1] == "Remnant" || data[i][1] == "Non-remnant")) {
-		var row = [data[i][1]]// clearing type
-		row = row.concat(data[i].slice(2));
-		table.push(row);
+
+	// chart 1 total clearing
+	heading = "Historic woody vegetation clearing in  " + regionName; // {1}
+	table = [["Type"]];
+	table[0] = table[0].concat(years.slice(10));
+
+	//body
+	for (var i = 1; i < data.length; ++i) {
+		if (data[i][0] == regionName && (data[i][1] == "Remnant" || data[i][1] == "Non-remnant")) {
+			var row = [data[i][1]]// clearing type
+			row = row.concat(data[i].slice(12));
+			table.push(row);
+		}
 	}
-}
 
-// iterate array for our html table
-tableHtml = tableToHtml(table, true);
+	// iterate array for our html table
+	var myTable = table.transpose();
+	tableHtml = tableToHtml(myTable, false);
 
-regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody, tableHtml.tfoot)
-print(regionInfo);
+	regionInfo = String.format(regionInfoTemplate, region, heading, ++index, tableHtml.thead, tableHtml.tbody)
+	print(regionInfo);
 
 
-// transpose for chart
-chart = { data: table.transpose() };
-chart.data[0][0] = { label: "Year", type: "string" };
-chart.options = getDefaultColumnChartOptions();
-chart.options.hAxis.title = "Year range";
-chart.options.vAxis.title = "Clearing rate (hectares per year)";
-chart.options.isStacked = true;
-chartData.push(chart);
+	// transpose for chart
+	chart = {
+		data: myTable,
+		type: "column"
+	};
+	chart.data[0][0] = { label: "Year", type: "string" };
+	chart.options = getDefaultColumnChartOptions();
+	chart.options.hAxis.title = "Year range";
+	chart.options.vAxis.title = "Clearing rate (hectares per year)";
+	chart.options.isStacked = true;
+	chartData.push(chart);
 
 
 
 });
 
 
-
-print("\n<script id=chartjson type=application/json>" + JSON.stringify(chartData) + "<" + "/script>\n");
+print("<script id=chartdata type=application/json>" + JSON.stringify(chartData) + "</" + "script>");
